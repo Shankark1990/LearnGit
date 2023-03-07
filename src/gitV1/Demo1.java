10ea7ed0b4508648f0c2edf9c5c2c4b671be3099
Shankar <shankarkharade1990@gmail.com>
2023-03-07 13:30:13 +0530

    My First commit :)

---
+++ src/gitV1/Demo1.java
@@ -0,0 +1,12 @@
+package gitV1;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My Git learn Project");
+
+	}
+
+}
